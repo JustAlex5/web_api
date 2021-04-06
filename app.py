5887907cd19f581235adbbeae03d542db89a2cf9
Alex Kovalyov <76871154+JustAlex5@users.noreply.github.com>
2021-04-06 23:40:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ class HelloWord(Resource):
 
 api.add_resource(HelloWord,"/helloword")
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(debug=True)
